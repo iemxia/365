2458c98d09a133a6972d69d235d09f7a0e37a3a6
iemxia <xmw2003@gmail.com>
2024-04-14 09:42:45 -0700

    bottler logic, <50 potions

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -62,25 +62,44 @@ def get_bottle_plan():
         green_ml = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).scalar_one()
         red_ml = connection.execute(sqlalchemy.text("SELECT num_red_ml FROM global_inventory")).scalar_one()
         blue_ml = connection.execute(sqlalchemy.text("SELECT num_blue_ml FROM global_inventory")).scalar_one()
+        green_potions_num = connection.execute(sqlalchemy.text("SELECT num_green_potions FROM global_inventory")).scalar_one()
+        blue_potions_num = connection.execute(sqlalchemy.text("SELECT num_blue_potions FROM global_inventory")).scalar_one()
+        red_potions_num = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).scalar_one()
+        total_potions = green_potions_num + blue_potions_num + red_potions_num
+        available_to_make = 50 - total_potions
+        potion_per_color = available_to_make // 3
         res = []
+        
         #[r, g, b, d]
-        if green_ml >= 100:
+        if green_ml >= 500:
             res.append(
                     {
                         "potion_type": [0, 100, 0, 0],
-                        "quantity": green_ml // 100,
+                        "quantity": 5,
                     })
-        if red_ml >= 100:
+        if red_ml >= (100 * potion_per_color):
             res.append(
                     {
                         "potion_type": [100, 0, 0, 0],
-                        "quantity": red_ml // 100
+                        "quantity": potion_per_color
                     })
-        if blue_ml >= 100:
+        elif red_ml >= 500:
+            res.append(
+                    {
+                        "potion_type": [100, 0, 0, 0],
+                        "quantity": 5
+                    })
+        if blue_ml >= (100 * potion_per_color):
             res.append(
                     {
                         "potion_type": [0, 0, 100, 0],
-                        "quantity": blue_ml // 100
+                        "quantity": potion_per_color
+                    })
+        elif blue_ml >= 500:
+            res.append(
+                    {
+                        "potion_type": [0, 0, 100, 0],
+                        "quantity": 5
                     })
     return res
 
