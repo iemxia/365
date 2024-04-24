b3b33f5df395bb0c0cb93cede4f22ef392ed753b
Xia Webster <xmw2003@gmail.com>
2024-04-24 09:24:26 -0700

    buy more capacity

--- src/api/inventory.py
+++ src/api/inventory.py
@@ -39,7 +39,7 @@ def get_capacity_plan():
         "potion_capacity": 2,
         "ml_capacity": 2
         }
-    elif gold >= 6000 and ml_cap <= 20000 and potion_cap <= 100:
+    elif gold >= 6000 and ml_cap <= 30000 and potion_cap <= 150:
         return {
         "potion_capacity": 1,
         "ml_capacity": 1
